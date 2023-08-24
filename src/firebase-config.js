06147dd9b0f85a1f7fb5e95733b8d74f0f948cec
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-24 20:35:51 +0530

    feat: updated new component

---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZLw8C6VGlrqtsuiOFlrlUvPQzQk8OkOk",
+  authDomain: "reference-repo.firebaseapp.com",
+  projectId: "reference-repo",
+  storageBucket: "reference-repo.appspot.com",
+  messagingSenderId: "182464643868",
+  appId: "1:182464643868:web:194506364e3c7391b770d9",
+  measurementId: "G-7BT5M2PJCF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import {React,useState} from 'react'
 import '../styles/new.css'
 import Header from '../components/Header'
 import {Breadcrumbs, Typography} from '@mui/material'
 import {Link} from 'react-router-dom';
+import { db } from '../firebase-config';
+import { collection, addDoc } from "firebase/firestore";
+
 
 function New() {
+  const [name, setName] = useState('');
+  const [author, setAuthor] = useState('');
+  const [year, setYear] = useState('');
+  const [category, setCategory] = useState('');
+  
+  const addNew=async()=>{
+    const docRef = await addDoc(collection(db, "references"), {
+        Name: name,
+        Author: author,
+        Category: category,
+        Year: year,
+        
+      }).then(()=>{
+        console.log("data added!")  
+      }).catch(()=>{
+        console.log("error!")  
+      });
+   
+}
+
+
+
   return (
     <div>
         <Header/>
@@ -25,14 +50,14 @@ function New() {
             <div className="row ">         
                 <div class="col">
                   <div class="form-outline">
-                    <input type="text" id="form8Example1" class="form-control" />
+                    <input type="text" id="form8Example1" class="form-control" value={name} onChange={(e)=>{setName(e.target.value)}}/>
                     <label className="form-label new" for="form8Example1">Reference Name</label>
                   </div>
                 </div>
                 <div class="col">
                 
                   <div class="form-outline">
-                    <input type="email" id="form8Example2" class="form-control" />
+                    <input type="text" id="form8Example2" class="form-control" value={author} onChange={(e)=>{setAuthor(e.target.value)}}/>
                     <label className="form-label new" for="form8Example2">Author</label>
                   </div>
                 </div>
@@ -43,14 +68,14 @@ function New() {
             <div class="row">
                 <div class="col">
                   <div class="form-outline">
-                    <input type="text" id="form8Example3" class="form-control" />
+                    <input type="text" id="form8Example3" class="form-control" value={category} onChange={(e)=>{setCategory(e.target.value)}}/>
                     <label className="form-label new" for="form8Example3">category</label>
                   </div>
                 </div>
                 <div class="col">
                   
                   <div class="form-outline">
-                    <input type="text" id="form8Example4" class="form-control" />
+                    <input type="text" id="form8Example4" class="form-control" value={year} onChange={(e)=>{setYear(e.target.value)}}/>
                     <label className="form-label new" for="form8Example4">Published Year</label>
                   </div>
                 </div>
@@ -66,7 +91,7 @@ function New() {
             
         </div>
         <div className="newButton">
-          <button className='addButton'>Add To Store</button>
+          <button className='addButton' onClick={addNew}>Add To Store</button>
         </div>
         </div>
     </div>
