f5c65dd18c1a11a1c7668d627070e8b4bb5fb67e
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-24 13:16:07 +0530

    feat: updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/header.css'
+import logo from '../images/logo.gif'
+
+
+function Header() {
+  return (
+    <div>
+        <div className="logo-container">
+            <img src={logo} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
